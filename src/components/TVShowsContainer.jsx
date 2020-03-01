dac9ac45b217fafbc60395fb8ec6256c1ede885e
Raymond Chow <rchow2@buffalo.edu>
2020-02-29 20:55:35 -0500

    Installed moment.js. Fetched schedule data for current date. Displayed names of TV shows in container.

--- src/components/TVShowsContainer.jsx
+++ src/components/TVShowsContainer.jsx
@@ -1,7 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
 
 const TVShowsContainer = () => {
-	return <div>Tv Shows Container</div>;
+	const [shows, setShows] = useState([]);
+
+	// Fetch Today's TV Shows
+	useEffect(() => {
+		const getSchedule = async date => {
+			const scheduleUrl = `http://api.tvmaze.com/schedule?country=US&date=${date}`;
+			const data = await fetch(scheduleUrl);
+			const todaysEpisodes = await data.json();
+			const todaysShows = todaysEpisodes.map(episode => episode.show);
+			setShows(todaysShows);
+		};
+
+		const today = moment().format("YYYY-MM-DD");
+		getSchedule(today);
+	}, []);
+
+	return (
+		<div>
+			<ul>
+				{shows.map(show => (
+					<li>{show.name}</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default TVShowsContainer;
