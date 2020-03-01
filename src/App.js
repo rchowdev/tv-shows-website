8f89bfb2c1a2e055ca161d851782986bf8fea426
Raymond Chow <rchow2@buffalo.edu>
2020-02-29 20:20:38 -0500

    Added components directory. Created Tv Shows container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import TVShowsContainer from "./components/TVShowsContainer";
+
 function App() {
-	return <div>Hello World</div>;
+	return (
+		<div>
+			<TVShowsContainer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/TVShowsContainer.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+
+const TVShowsContainer = () => {
+	return <div>Tv Shows Container</div>;
+};
+
+export default TVShowsContainer;
